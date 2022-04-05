f1744f160145e2385cde8805be626477501423b0
volchishka1 <romazverb18@gmail.com>
2022-04-05 14:53:46 +0300

    set redux for web

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,19 +1,29 @@
-import { usersAPI } from "@footballer/api";
-import { myString } from "@footballer/common";
+import {decrement, increment} from "@footballer/redux/src/toolkitReducer";
+import {usersAPI} from "@footballer/api";
+import {myString} from "@footballer/common";
+// @ts-ignore
 import React from "react";
 // @ts-ignore
 import logo from "./logo.svg";
 import "./App.css";
+import {useDispatch, useSelector} from "react-redux";
 
 
 function App() {
+    // @ts-ignore
+    const count = useSelector(state => state.toolkit.count);
+    const dispatch = useDispatch();
+
     // eslint-disable-next-line no-console
-    console.log(usersAPI(`${process.env.REACT_APP_BASE_URL}`,`${process.env.REACT_APP_APIKEY}`).getMatches);
+    console.log(usersAPI(`${process.env.REACT_APP_BASE_URL}`, `${process.env.REACT_APP_APIKEY}`).getMatches);
     return (
         <div className="App">
             <header className="App-header">
-                <img src={ logo } className="App-logo" alt="logo" />
-                { myString() }
+                <h1>Счетчик {count}</h1>
+                <img src={logo} className="App-logo" alt="logo"/>
+                <button onClick={() => dispatch(increment())}>Инкремент</button>
+                <button onClick={() => dispatch(decrement())}>Декремент</button>
+                {myString()}
             </header>
         </div>
     );
--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,12 +1,19 @@
+// @ts-ignore
 import React from "react";
+// @ts-ignore
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import {Provider} from "react-redux";
+import {store} from "@footballer/redux";
+
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </React.StrictMode>,
     document.getElementById("root")
 );
