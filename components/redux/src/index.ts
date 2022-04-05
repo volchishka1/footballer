8c53bf0f2dc45127878a4d77469e72f349484b30
volchishka1 <romazverb18@gmail.com>
2022-04-05 15:28:53 +0300

    set redux for web

--- components/redux/src/index.ts
+++ components/redux/src/index.ts
@@ -1,8 +1,8 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import toolkitReducer from "./toolkitReducer";
+import toolkitSlice from "./toolkitSlice";
 
 const rootReducer = combineReducers( {
-  toolkit: toolkitReducer
+  toolkit: toolkitSlice
 })
 
 export const store = configureStore( {
