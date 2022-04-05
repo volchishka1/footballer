e64a2a0b9dfa072ca6e157c8c2555f5cfd501738
volchishka1 <romazverb18@gmail.com>
2022-04-05 11:18:14 +0300

    end set api mobile

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -2,13 +2,8 @@ const path = require('path');
 const watchFolders = [
   //Relative path to packages directory
   // eslint-disable-next-line no-path-concat
-  path.resolve(__dirname + '../../components'),
+  path.resolve(__dirname + '../../../components'),
 ];
-// require('dotenv').config({
-//   path: path.join(process.cwd(), '..', '..', '..', '.env'),
-// });
-//
-// module.exports = process.env;
 
 module.exports = {
   transformer: {
